b75cee93ca0fbcbaf987187a16dd3058d1641c8d
SirPakun <mafburgmufbarg@gmail.com>
2025-08-21 10:46:44 +0300

    First Commit

---
+++ src/main/java/by/kirienko/utils/CabinetStatistic.java
@@ -0,0 +1,17 @@
+package by.kirienko.utils;
+
+import entities.CabinetEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CabinetStatistic {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CabinetStatistic.class);
+
+    public static void showStatistic() {
+        for (CabinetEnum cabinetEnum : CabinetEnum.values()) {
+            LOGGER.info("Cabinet {}, max patient : {}; serviced patients : {}; leaved patients : {}",
+                    cabinetEnum.name(), cabinetEnum.getMaxServiceNumber(),
+                    cabinetEnum.getServicedPatients(), cabinetEnum.getLeavedPatients());
+        }
+    }
+}
